f2e22cb655d0ed5f8858ddc1844c8f5a46df5cc1
Raphael Serota <raphael.serota@gmail.com>
2018-05-04 17:35:29 -0600

    componentized modals

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,44 @@
-console.log('hello!')
+Vue.component('login-modal', function(resolve, reject){
+    axios.get('/html/components/login-modal.component.html').then(function(response){
+        console.log(response)
+        resolve({
+            template: response.data
+        })
+    })
+})
+Vue.component('signup-modal', function(resolve, reject){
+    axios.get('/html/components/signup-modal.component.html').then(function(response){
+        console.log(response)
+        resolve({
+            template: response.data
+        })
+    })
+})
+Vue.component('create-a-dish', function(resolve, reject){
+    axios.get('/html/components/create-a-dish.component.html').then(function(response){
+        console.log(response)
+        resolve({
+            template: response.data
+        })
+    })
+})
 
+
+
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: {
+                template: '#home-template'
+            }
+        }
+    ]
+})
 // creating a View-Model for the list
 var mainVm = new Vue({
     el: '#app',
+    router: router,
     data: {
         user: {},
         alerts    : [],
@@ -16,6 +52,10 @@ var mainVm = new Vue({
                 email: 'jan.smith@gmail.com',
                 password:'dragons'
             },
+            createDishForm: {
+                name: 'Lasagna',
+                description: "Just like grandma used to make.\n\nServes 3 to 5."
+            }
         }
     },
     computed: {
